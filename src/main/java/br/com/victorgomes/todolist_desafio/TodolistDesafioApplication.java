e4b7e230c3e3549033723b6f8dd2f3d774cb1438
Victor Hugo <victor.couto@ufrpe.br>
2025-11-04 21:43:48 -0300

    criando entidade Todo

---
+++ src/main/java/br/com/victorgomes/todolist_desafio/TodolistDesafioApplication.java
@@ -0,0 +1,13 @@
+package br.com.victorgomes.todolist_desafio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodolistDesafioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodolistDesafioApplication.class, args);
+	}
+
+}
---
+++ src/main/java/br/com/victorgomes/todolist_desafio/entities/Todo.java
@@ -0,0 +1,55 @@
+package br.com.victorgomes.todolist_desafio.entities;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "todos")
+public class Todo {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nome;
+    private String descricao;
+    private boolean feito;
+    private int prioridade;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public boolean isFeito() {
+        return feito;
+    }
+
+    public void setFeito(boolean feito) {
+        this.feito = feito;
+    }
+
+    public int getPrioridade() {
+        return prioridade;
+    }
+
+    public void setPrioridade(int prioridade) {
+        this.prioridade = prioridade;
+    }
+}
